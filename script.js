1e093ce1a27f332603d42a5cdb7e39f24f10df83
murad <murad@gmail.com>
2023-12-08 20:07:57 +0100

    aaa

---
+++ script.js
@@ -0,0 +1,23 @@
+let login = prompt('Bura logininizi yazin.');
+let password = prompt('Bura parolunuzu yazin.');
+let netice;
+
+switch (login, password) {
+    case (login = 'admin') && (password = '123456'):
+        netice = 'Salam Admin! Sayti redakte ede bilersiniz.'
+        break;
+    case (login = 'ayaz') && (password = '123456'):
+        netice = 'Xos geldin, Ayaz!'
+        break;
+    case (login = 'murad') && (password = 'murik1234'):
+        netice = 'Xos geldin, Murad!'
+        break;
+    case  (login = 'mika') && (password = 'mika123'):
+        netice = 'Xos geldin, Mikayil!'
+        break;
+    default:
+        netice = 'Login ve ya parol sehvdir.'
+    break;
+}
+
+document.getElementById('murad').innerHTML = netice;
